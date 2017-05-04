64b2c3fb300ec5249a2597a52af0100e890448b8
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-03 23:08:23 -0700

    has button but doesn't show count

--- react-sample-app/src/App.js
+++ react-sample-app/src/App.js
@@ -6,6 +6,19 @@ import './App.css';
 class Hello extends Component {
   // Components let you split the UI into independent, reusable pieces
   // They accept arbitrary inputs (called "props") and return React elements describing what should appear on the screen.
+  constructor (props) {
+    // make call to parent class' (Component) constructor// NEED CONSTRUCTOR TO Setting up and modifying state
+    super()
+    this.state = {
+      counter: 0  // initialize this.state.counter to be 0
+    }
+  }
+  handleClick(e) {
+    // need some trigger event to change counter
+    this.setState({  // setState is inherited from the Component class
+      counter: this.state.counter +1
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -15,14 +28,16 @@ class Hello extends Component {
           <img src='https://logopond.com/logos/2604a0c4c5ede1bf3f2f999e145ae960.png' className="App-logo" alt="logo" />
           <h2>Hello {this.props.name}!</h2>
           <p>and you are {this.props.age} years old</p>
-          
+          {/*  so props CAN be data - but doesn't change until page reloads*/}
+          <p>Initial count is {this.state.counter}</p>
+          <button onClick={(e) => this.handleClick(e)}>Click  counter</button>
         </div>
         <p className="App-intro">
           Simon says: "To get started, edit <code>src/App.js</code> and save to reload."
         </p>
       </div>
     );
-  }
-}
+  }  //  render()
+}  // class Hello
 
 export default Hello;
